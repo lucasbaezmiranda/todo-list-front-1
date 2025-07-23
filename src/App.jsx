7eba63032385c8b8a1024e1afd34110e4fb3c7ca
Lucas Báez Miranda <lucas.baezmiranda@gmail.com>
2025-07-23 12:56:25 -0300

    segunda prueba func seno

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import MathGraph from './components/MathGraph';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   return (
     <div>
-      <h1>Todo List</h1>
+      <h1>Todo List v2</h1>
+      <MathGraph />
       <TodoForm onNewTodo={todo => setTodos(prev => [...prev, todo])} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
